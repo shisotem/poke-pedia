f3c56227c2ab3876fe0330161b050fd2637f7bc8
shisotem <129283841+shisotem@users.noreply.github.com>
2023-12-02 13:19:57 +0900

    feat(app): add loading state to App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,22 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import { getAllPokemon } from "./utils/pokemon";
 
 function App() {
   const initialURL = "https://pokeapi.co/api/v2/pokemon";
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const fetchPokemonData = async () => {
       // get all pokemon data
       const response = await getAllPokemon(initialURL);
       console.log(response);
+      setLoading(false);
     };
     fetchPokemonData();
   }, []);
 
-  return <></>;
+  return <>{loading ? <h1>Loading...</h1> : <h1>Data loaded!</h1>}</>;
 }
 
 export default App;
